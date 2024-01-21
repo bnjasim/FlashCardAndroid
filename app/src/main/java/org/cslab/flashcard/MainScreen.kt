45042e497ecb87120436692812c9e0b129f25fd5
bnjasim <bnjasim@gmail.com>
2024-01-22 02:34:27 +0530

    Display App title UniQuiz at the top of MainScreen

--- app/src/main/java/org/cslab/flashcard/MainScreen.kt
+++ app/src/main/java/org/cslab/flashcard/MainScreen.kt
@@ -1,9 +1,11 @@
 package org.cslab.flashcard
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -11,6 +13,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,28 +28,51 @@ fun MainScreen(navController: NavHostController) {
         QuizFile(R.raw.current_affairs, "Current Affairs 2023"),
         QuizFile(R.raw.gre, "GRE Words")
     )
-    // Display the Names as clickable Cards/Buttons
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        items(quizFiles) { quizFile ->
-            Card(
+    Column {
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 32.dp, horizontal = 24.dp),
+        ) {
+            Text(
+                text = "UniQuiz",
                 modifier = Modifier
+                    .padding(vertical = 16.dp)
                     .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable {
-                        // Directly call the composable function for the details screen
-                        navController.navigate("show_quiz/${quizFile.id}")
-                    }
-            ) {
-                // Get name
-                Text(
-                    text = quizFile.name,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontSize = 24.sp
-                )
+                    .wrapContentHeight(),
+                style = MaterialTheme.typography.headlineLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary
+                ),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        // Display the Names as clickable Cards/Buttons
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(quizFiles) { quizFile ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .clickable {
+                            // Directly call the composable function for the details screen
+                            navController.navigate("show_quiz/${quizFile.id}")
+                        }
+                ) {
+                    // Get name
+                    Text(
+                        text = quizFile.name,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
             }
         }
     }
