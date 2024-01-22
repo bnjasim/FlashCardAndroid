3e953fd99039533f0a27b9f131f071644abb9633
bnjasim <bnjasim@gmail.com>
2024-01-22 05:43:49 +0530

    Question, Answer and user answer areas in QuizFCard file

--- app/src/main/java/org/cslab/flashcard/QuizFCard.kt
+++ app/src/main/java/org/cslab/flashcard/QuizFCard.kt
@@ -1,14 +1,20 @@
 package org.cslab.flashcard
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -16,13 +22,16 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.cslab.flashcard.ui.theme.FlashCardTheme
 import kotlin.random.Random
 
@@ -42,6 +51,8 @@ fun QuizFCard(quizList: List<Pair<String, String>>) {
     var successRate by remember { mutableFloatStateOf(1F) }
     // keep track of the number of questions finished/learned
     var numQuizDone by remember { mutableIntStateOf(0) }
+    // User answer
+    var userAnswer by remember { mutableStateOf("") }
 
     Column (
         modifier = Modifier
@@ -92,6 +103,89 @@ fun QuizFCard(quizList: List<Pair<String, String>>) {
                 modifier = Modifier
                     .padding(start = 8.dp)
             )
+        }
+
+        // sample a quiz
+        val quizIndex = sampleIndex(quizWeights)
+        val quiz = quizList[quizIndex]
+        question = if (quizWeights.sum() == 0) {
+            "Congratulations!\n The Quiz is finished!"
+        } else {
+            quiz.first
+        }
+        // Display the Question Area
+        Card (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .height(150.dp)
+        ) {
+            Text(
+                text = question,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .padding(12.dp)
+            )
+        }
+        // Answer Area
+        Card (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .height(150.dp)
+        ) {
+            Text(
+                text = answer,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .padding(12.dp)
+            )
+        }
+
+        // Spacer to add vertical space
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // if answer is too long, only require any 4 consecutive characters
+        if (quiz.second.length > 4) {
+            // Display a small info text on top of the text field
+            Text(
+                text = "Any consecutive 4 characters",
+                color = Color.Gray,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .padding(start = 12.dp, top = 12.dp)
+            )
+        }
+
+        // An area to enter answer
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // User TextField
+            TextField(
+                value = userAnswer,
+                onValueChange = { userAnswer = it },
+                label = { Text("Answer") },
+                modifier = Modifier.weight(1f)
+            )
+
+            // Go Button
+            Button(
+                onClick = { /* onGoClick(userInput) */ },
+                modifier = Modifier
+                    .padding(start = 8.dp),
+                shape = MaterialTheme.shapes.small
+            ) {
+                Text(
+                    text = "Go",
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
         }
     }
 }
